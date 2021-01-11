c27539a375b1e39411278fbe0966914400426669
sjproctor <sjproctor950@gmail.com>
2021-01-11 15:25:09 -0800

    adds update and delete request specs and endpoints

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'should have a valid name' do
+    cat = Cat.create(age: 4, enjoys: 'Walks on the beach')
+    expect(cat.errors[:name]).to_not be_empty
+  end
+
+  it 'should have a valid age' do
+    cat = Cat.create(name: 'Neil', enjoys: 'Walks on the beach')
+    expect(cat.errors[:age]).to_not be_empty
+  end
+
+  it 'should have a valid enjoys' do
+    cat = Cat.create(name: 'Neil', age: 4)
+    expect(cat.errors[:enjoys]).to_not be_empty
+  end
+
+  it 'should have an enjoys attribute with at least 10 characters' do
+    cat = Cat.create(name: 'Felix', age: 7, enjoys: 'Walks')
+    expect(cat.errors[:enjoys]).to_not be_empty
+  end
+
 end
--- spec/requests/cats_request_spec.rb
+++ spec/requests/cats_request_spec.rb
@@ -9,12 +9,10 @@ RSpec.describe "Cats", type: :request do
 
     # Make a request to the API
     get '/cats'
-
     cats = JSON.parse(response.body)
 
-    expect(response).to have_http_status(200)
     expect(cats.length).to eq 1
-
+    expect(response).to have_http_status(200)
   end
 
   # create
@@ -23,18 +21,102 @@ RSpec.describe "Cats", type: :request do
       cat: {
         name: 'Larry',
         age: 8,
-        enjoys: 'Sleeping'
+        enjoys: 'Sleeping on your face'
       }
     }
-
     # Make a request to the API
     post '/cats', params: cat_params
-
-    expect(response).to have_http_status(200)
-
     cat = Cat.first
-    expect(cat.name).to eq 'Larry'
 
+    expect(cat.name).to eq 'Larry'
+    expect(response).to have_http_status(200)
+  end
+
+  # update
+  it 'edits a cat' do
+    cat_params = {
+      cat: {
+        name: 'Felix',
+        age: 4,
+        enjoys: 'Walks in the park.'
+      }
+    }
+    post '/cats', params: cat_params
+    cat = Cat.first
+
+    new_cat_params = {
+      cat: {
+        name: 'Felix',
+        age: 2,
+        enjoys: 'Walks in the park.'
+      }
+    }
+    patch "/cats/#{cat.id}", params: new_cat_params
+    cat = Cat.find cat.id
+
+    expect(cat.age).to eq 2
+    expect(response).to have_http_status(200)
+  end
+
+
+  # delete
+  it 'deletes a cat' do
+    cat_params = {
+      cat: {
+        name: 'Felix',
+        age: 4,
+        enjoys: 'Walks in the park.'
+      }
+    }
+    post '/cats', params: cat_params
+    cat = Cat.first
+    delete "/cats/#{cat.id}"
+    cats = Cat.all
+
+    expect(cats).to be_empty
+    expect(response).to have_http_status(200)
+  end
+
+  it 'cannot create a new cat without a name' do
+    cat_params = {
+      cat: {
+        age: 8,
+        enjoys: 'Sleeping on your face'
+      }
+    }
+    post '/cats', params: cat_params
+    json = JSON.parse(response.body)
+
+    expect(json['name']).to include "can't be blank"
+    expect(response).to have_http_status(422)
+  end
+
+  it 'cannot create a new cat without an age' do
+    cat_params = {
+      cat: {
+        name: 'Neil',
+        enjoys: 'Watching the goldfish bowl'
+      }
+    }
+    post '/cats', params: cat_params
+    json = JSON.parse(response.body)
+
+    expect(json['age']).to include "can't be blank"
+    expect(response).to have_http_status(422)
+  end
+
+  it 'cannot create a new cat without an enjoys' do
+    cat_params = {
+      cat: {
+        name: 'Neil',
+        age: 4
+      }
+    }
+    post '/cats', params: cat_params
+    json = JSON.parse(response.body)
+
+    expect(json['enjoys']).to include "can't be blank"
+    expect(response).to have_http_status(422)
   end
 
 end
